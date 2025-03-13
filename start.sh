8fb5bf61d6e2e7aaa408c16872377f6066ec902b
Evan Jackson <evan@jacksonnet.ca>
2025-03-13 18:02:08 -0400

    Fix plugin looping

--- start.sh
+++ start.sh
@@ -6,11 +6,12 @@ echo "Setting UID and GID of minecraft user..."
 usermod -u $PUID minecraft
 groupmod -g $PGID minecraft
 
-echo "Adding custom plugins..."
-echo "$CUSTOM_PLUGIN_URLS" | tr ',' '\n' | while read -r plugin; do
-  echo "Adding $plugin..."
-  wget -P /home/minecraft/plugins $plugin 
+IFS=',' read -ra PLUGINS <<< "$CUSTOM_PLUGIN_URLS"
+for plugin in "${PLUGINS[@]}"; do
+  echo "Downloading $plugin..."
+  wget -P /home/minecraft/plugin $plugin
 done
+
 
 echo "Starting Minecraft (via Spigot)..."
 java $JAVA_OPTIONS -jar /home/minecraft/spigot-$MINECRAFT_VERSION.jar --nogui
